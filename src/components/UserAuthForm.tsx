68ad05f92a7ed16d199f65a8c1ec97922266dd6f
SaiRev <saiyamjain1234567@gmail.com>
2025-05-01 00:36:19 +0530

    signin issue

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -3,8 +3,6 @@
 import * as React from 'react';
 import { FC } from 'react';
 
-import { useSearchParams } from 'next/navigation';
-
 import { Icons } from '@/components/Icons';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
@@ -16,54 +14,41 @@ import { toast } from 'sonner';
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
-    const searchParams = useSearchParams();
-    const [isLoading, setIsLoading] = React.useState<boolean>(false);
-    const [signInUrl, _, removeSignInUrl] = useLocalStorage<string>({
-        key: 'redirectURLfromSignIn',
-        defaultValue: '/'
-    });
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [signInUrl, _, removeSignInUrl] = useLocalStorage<string>({
+    key: 'redirectURLfromSignIn',
+    defaultValue: '/'
+  });
 
-    const search = searchParams.get('error');
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
 
-    React.useEffect(() => {
-        if (!search) {
-            return;
-        } else {
-            toast.error('Error', {
-                description: 'There was an error logging in with Google'
-            });
-        }
-    }, [search]);
+    try {
+      await signIn('google', { callbackUrl: signInUrl });
+    } catch (error) {
+      toast.error('Error', {
+        description: 'There was an error logging in with Google'
+      });
+    } finally {
+      setIsLoading(false);
+      removeSignInUrl();
+    }
+  };
 
-    const loginWithGoogle = async () => {
-        setIsLoading(true);
-
-        try {
-            await signIn('google', { callbackUrl: signInUrl });
-        } catch (error) {
-            toast.error('Error', {
-                description: 'There was an error logging in with Google'
-            });
-        } finally {
-            setIsLoading(false);
-            removeSignInUrl();
-        }
-    };
-
-    return (
-        <div className={cn('flex justify-center', className)} {...props}>
-            <Button
-                isLoading={isLoading}
-                type='button'
-                size='sm'
-                className='w-full'
-                onClick={loginWithGoogle}
-                disabled={isLoading}>
-                {isLoading ? null : <Icons.google className='mr-2 h-4 w-4' />}
-                Google
-            </Button>
-        </div>
-    );
+  return (
+    <div className={cn('flex justify-center', className)} {...props}>
+      <Button
+        isLoading={isLoading}
+        type='button'
+        size='sm'
+        className='w-full'
+        onClick={loginWithGoogle}
+        disabled={isLoading}>
+        {isLoading ? null : <Icons.google className='mr-2 h-4 w-4' />}
+        Google
+      </Button>
+    </div>
+  );
 };
 
 export default UserAuthForm;
